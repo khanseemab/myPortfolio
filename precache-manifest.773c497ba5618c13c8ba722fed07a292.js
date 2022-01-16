ff511d0781b0d5b36626cc518c6f15467ce4605c
seemab2703@gmail.com <seemab2703@gmail.com>
2022-01-16 22:42:08 +0530

    Updates

---
+++ precache-manifest.773c497ba5618c13c8ba722fed07a292.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72feae7254d5f6323eb56c3eab7c51f8",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "8eb660b0e8ab47ac0fbf",
+    "url": "/myPortfolio/static/css/main.e76a8ddd.chunk.css"
+  },
+  {
+    "revision": "b6f4678308c2e9b90469",
+    "url": "/myPortfolio/static/js/2.5e2cd343.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/myPortfolio/static/js/2.5e2cd343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb660b0e8ab47ac0fbf",
+    "url": "/myPortfolio/static/js/main.40b0e467.chunk.js"
+  },
+  {
+    "revision": "d2cbd7fa1ae2cfd92fb1",
+    "url": "/myPortfolio/static/js/runtime-main.a2f877e0.js"
+  },
+  {
+    "revision": "4f43aba5d43bb63e282342ae4caa2e0c",
+    "url": "/myPortfolio/static/media/7038048-purple-dark-smoke.4f43aba5.jpg"
+  },
+  {
+    "revision": "013dbf9d5b821e999dde91a4d5843da8",
+    "url": "/myPortfolio/static/media/Seemab's_Resume.013dbf9d.pdf"
+  },
+  {
+    "revision": "ae48805f519b3ccf811efc6d6d2f8c99",
+    "url": "/myPortfolio/static/media/clock.ae48805f.png"
+  },
+  {
+    "revision": "f5b0453f1a7512e9c04cb4790ed12ad0",
+    "url": "/myPortfolio/static/media/paymentform.f5b0453f.png"
+  },
+  {
+    "revision": "36b406c197395dd4effccd7ee92ae0af",
+    "url": "/myPortfolio/static/media/profileimage.36b406c1.jpg"
+  }
+]);
